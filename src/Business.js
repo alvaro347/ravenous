f9fca492ea6a187e905e2c60f511f29db7054407
Alvaro <alvaro347@gmail.com>
2024-03-22 22:42:02 +0000

    Added Business base component

---
+++ src/Business.js
@@ -0,0 +1,15 @@
+const business = (
+    <div>
+        <img alt="business"></img>
+        <p>name</p>
+        <p>Address</p>
+        <p>City</p>
+        <p>State</p>
+        <p>Zip code</p>
+        <p>Category</p>
+        <p>Rating</p>
+        <p>Review</p>
+    </div>
+);
+
+export default business
