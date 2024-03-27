6c798e6f0cf272fa1f4515c40a8b15f22c69c532
Alvaro <alvaro347@gmail.com>
2024-03-27 15:29:51 +0000

    Business display cards CSS improved

--- src/Components/Business.js
+++ src/Components/Business.js
@@ -3,20 +3,21 @@ import React from "react";
 function Business({ business }) {
 	return (
 		<div className="business-card">
-			<div>
-				<img alt="business" src={ business.imageSrc } className="business-img"></img>
-			</div>
-			<h3 className="business-title">{ business.name }</h3>
-			<div>
-				<p>{ business.address }</p>
-				<p>{ business.city }</p>
-				<p>{ business.state }</p>
-				<p>{ business.zipCode }</p>
-				<p>{ business.category }</p>
-				<p>{ business.rating }</p>
-			</div>
-			<div>
-				<p>Review</p>
+			<img alt="business" src={business.imageSrc} className="business-img"></img>
+			<div className="business-info">
+				<h3 className="business-title">{business.name}</h3>
+				<div className="business-text-grid">
+					<div className="business-grid-1">
+						<p>{business.address}</p>
+						<p>{business.city}</p>
+						<p>{business.state}, {business.zipCode}</p>
+					</div>
+					<div className="business-grid-2">
+						<p className="business-category">{business.category}</p>
+						<p>{business.rating} Stars</p>
+						<p>Review</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
